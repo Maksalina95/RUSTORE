19a4fd8a89af9864ae3f021b4a5f0d57706ab9cc
Maksalina95 <maksalinamagazin@gmail.com>
2025-12-09 13:52:03 +0300

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,5 +1,7 @@
 package com.maksmarket.app
 
+import android.content.Intent
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -11,9 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Проверка интернета
+        val cm = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = cm.activeNetworkInfo
+        if (networkInfo == null || !networkInfo.isConnected) {
+            startActivity(Intent(this, OfflineActivity::class.java))
+            finish()
+            return
+        }
+
         val webView: WebView = findViewById(R.id.webview)
         webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = true
-        webView.loadUrl("https://maksmarket.vercel.app") // твой сайт
+        webView.loadUrl("https://maksmarket.vercel.app") // Твой сайт
     }
 }
