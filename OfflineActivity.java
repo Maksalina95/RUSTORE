7a0f1e5953d8285aea86500ba6dd1b9871f99d81
Maksalina95 <maksalinamagazin@gmail.com>
2025-12-09 13:37:54 +0300

    Create OfflineActivity.java

---
+++ OfflineActivity.java
@@ -0,0 +1,13 @@
+package com.maksmarket.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OfflineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offline);
+    }
+}
