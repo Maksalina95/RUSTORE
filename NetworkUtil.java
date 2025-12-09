20c2caaae184bc89bb1754c41e2eb4864ac346a5
Maksalina95 <maksalinamagazin@gmail.com>
2025-12-09 13:38:40 +0300

    Create NetworkUtil.java

---
+++ NetworkUtil.java
@@ -0,0 +1,18 @@
+package com.maksmarket.app;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkUtil {
+
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager cm = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm != null) {
+            NetworkInfo ni = cm.getActiveNetworkInfo();
+            return ni != null && ni.isConnected();
+        }
+        return false;
+    }
+}
