88ccf805b64e58e058dc29dc296f80a54b12e4c9
Maksalina95 <maksalinamagazin@gmail.com>
2025-12-09 13:36:49 +0300

    Create SplashActivity.java

---
+++ SplashActivity.java
@@ -0,0 +1,22 @@
+package com.maksmarket.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }, SPLASH_DELAY);
+    }
+}
